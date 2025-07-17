577bdc538057600de9b964fa6940197d29bc9baa
ZenithForex-Navnit <it.developer3@zenithforex.com>
2025-07-17 16:36:37 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -59,8 +59,8 @@ app.use('/api/upload', uploadRoute);
 
 
 // 🔌 Redis Pub/Sub Clients
-// const pubClient = createClient({ url: 'redis://redis:6379' });
-const pubClient = createClient({ url: 'redis://localhost:6379' });
+const pubClient = createClient({ url: 'redis://redis:6379' });
+// const pubClient = createClient({ url: 'redis://localhost:6379' });
 const subClient = pubClient.duplicate();
 
 // ⚡️ Socket.IO Setup
